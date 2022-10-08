6e27c22d69d555b556d029492a8555f7317e1078
nicey <laisi1127@163.com>
2022-10-08 18:02:05 +0800

    修改 grid

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -10,12 +10,7 @@ export const Chart2 = () => {
     var myChart = echarts.init(divRef.current);
     myChart.setOption(createEchartsOptions({
       ...baseEchartOptions,
-      grid: {
-        x: px(100),
-        y: px(40),
-        x2: px(40),
-        y2: px(40),
-      },
+
       xAxis: {
         type: 'value',
         boundaryGap: [0, 0.01]
