18baf0aac7088f00e3c61e7d0fbd2f37442b8854
nicey <laisi1127@163.com>
2022-10-09 11:53:36 +0800

    制作 Chart4

---
+++ src/components/chart-4.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useRef} from 'react';
+import * as echarts from 'echarts';
+import {createEchartsOptions} from '../shared/create-echarts-options';
+import {px} from '../shared/px';
+
+export const Chart4 = () => {
+  const divRef = useRef(null);
+  useEffect(() => {
+    var myChart = echarts.init(divRef.current);
+    myChart.setOption(createEchartsOptions({
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
+          splitLine: {show: true, lineStyle: {color: '#073E78'}},
+        axisTick: {show: false},
+        axisLine: { show: false },
+      },
+      
+      yAxis: {
+        splitLine: {lineStyle: {color: '#073E78'}},
+        type: 'value',
+        axisLabel: {
+          formatter(val) {
+            return val * 100 + '%';
+          }
+        }
+      },
+      series: [
+        { name: '故意伤人',
+        type: 'line',
+          data: [0.15, 0.13, 0.12,0.13,0.14,0.16,0.18,0.25,0.27,0.28,0.26,0.20,0.14],  symbol: 'circle',
+        symbolSize: px(12),
+        lineStyle: {width: px(2)},
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+            offset: 0,
+            color: '#414a9f'
+          }, {
+            offset: 1,
+            color: '#1b1d52'
+          }]),
+        }
+      }]
+    }));
+  }, []);
+
+
+  return (
+    <div className="bordered 发案趋势">
+      <h2>发案趋势分析</h2>
+      <div ref={divRef} className="chart"/>
+    </div>
+  );
+};
