3c368cc60bda1e4abf1186a2b0153f578663e48b
nicey <laisi1127@163.com>
2022-10-10 16:46:16 +0800

    实时更新chart10

--- src/components/chart-10.tsx
+++ src/components/chart-10.tsx
@@ -4,11 +4,32 @@ import {createEchartsOptions} from '../shared/create-echarts-options';
 
 export const Chart10 = () => {
   const divRef = useRef(null);
-  useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    myChart.setOption(createEchartsOptions({
+  const myChart = useRef(null);
+  const data =
+    
+    [{ name: '入室抢劫', 2011: 40 },
+    { name: '当街偷盗', 2011: 22 },
+    { name: '团伙诈骗', 2011: 20 },
+    { name: '刑事案件', 2011: 18 },
+    { name: '民事案件', 2011: 32 },
+    ];   
+    useEffect(() => {
+    setInterval(() => {
+      const newData = [
+   { name: '入室抢劫', 2011: Math.random() * 100 },
+    { name: '当街偷盗', 2011: Math.random() * 100 },
+    { name: '团伙诈骗', 2011: Math.random() * 100 },
+    { name: '刑事案件', 2011: Math.random() * 100 },
+    { name: '民事案件', 2011: Math.random() * 100 },
+      ];
+      x(newData);
+    }, 2000);
+    }, []);
+  
+  const x = (data) => { 
+   myChart.current.setOption(createEchartsOptions({
       xAxis: {
-        data: ['入室抢劫', '当街偷盗', '团伙诈骗', '刑事案件', '民事案件'],
+                      data: data.map(i => i.name),
         axisTick: {show: false},
         axisLine: {
           lineStyle: {color: '#083B70'}
@@ -35,21 +56,26 @@ export const Chart10 = () => {
       },
       series: [{
         type: 'bar',
-        data: [40, 22, 20, 18, 32],
+          data: data.map(i => i[2011]),
+    
         color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
           offset: 0,
-          color: '#0A97FB'
+          color: '#0A67FF'
         }, {
           offset: 1,
-          color: '#1E34FA'
+          color: '#B92AE8'
         }]),
       }]
     }));
+
+  }
+  useEffect(() => {
+     myChart.current  = echarts.init(divRef.current);
+ 
   }, []);
 
   return (
     <div ref={divRef} className="chart">
-
     </div>
   );
 };
