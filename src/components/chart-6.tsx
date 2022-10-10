fcaa2d8e468c4bf0b6ef5ce62d76a4e1d2e23850
nicey <laisi1127@163.com>
2022-10-10 14:20:16 +0800

    更新 .chart11 和 .chart12

--- src/components/chart-6.tsx
+++ src/components/chart-6.tsx
@@ -75,9 +75,9 @@ export const Chart6 = () => {
       <div className="wrapper">
         <div ref={divRef} className="chart"/>
         <div className="legend bordered">
-          <span className="icon" style={{background: colors['甘肃省']}}/>甘肃籍
-          <span className="icon" style={{background: colors['四川省']}}/>四川籍
-          <span className="icon" style={{background: colors['青海省']}}/>青海籍
+          <span className="icon" style={{background: colors['甘肃省']}}/>边城籍
+          <span className="icon" style={{background: colors['四川省']}}/>清水籍
+          <span className="icon" style={{background: colors['青海省']}}/>柑岭籍
         </div>
         <div className="notes">此地图仅显示了中国的部分区域</div>
       </div>
